484f38f964842dd703abe3d4ac44c7fe29e559ed
atifali78 <atif7803320@gmail.com>
2024-08-24 11:03:02 +0500

    Start my login page step 1

---
+++ src/loginPage/loginPage.jsx
@@ -0,0 +1,12 @@
+// Import necessary modules
+import React from 'react';
+
+
+// Render the React application
+const loginPage = () => {
+    return (
+        <div>LoginPAge</div>
+    )
+}
+    
+  export default loginPage
